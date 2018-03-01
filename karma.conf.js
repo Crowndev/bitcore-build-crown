2f2c08be4675197ce6f63b9b87b0351d19f24c63
Zhenzhen Zhan <zhanzhenzhen@hotmail.com>
2018-03-01 22:31:23 +0800

    Drop phantomjs, use puppeteer

--- karma.conf.js
+++ karma.conf.js
@@ -1,10 +1,11 @@
 'use strict';
 
-// karma.conf.js
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
 module.exports = function(config) {
 
   config.set({
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     frameworks: ['mocha'],
     singleRun: true,
     files: [
@@ -12,7 +13,7 @@ module.exports = function(config) {
     ],
     plugins: [
       'karma-mocha',
-      'karma-phantomjs-launcher'
+      'karma-chrome-launcher'
     ]
   });
 
